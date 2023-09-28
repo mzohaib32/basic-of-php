baa3a9f277b9b75c97a78b8385d20e75e1edd034
mzohaib32 <146351776+mzohaib32@users.noreply.github.com>
2023-09-28 19:39:20 +0500

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,17 @@
+<?php
+$a=20;
+$b=5;
+$opr="+";
+if($opr=="+")
+echo $a+$b;
+elseif($opr=="-")
+echo $a-$b;
+elseif($opr=="*")
+echo $a*$b;
+else if($opr=="/")
+echo $a/$b;
+else if($opr=="%")
+echo $a%$b;
+else
+echo "invalid";
+?>
